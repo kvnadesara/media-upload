5edc9959a4ade34f37f5cbfc2f4915cedaf7db81
Kevin Adesara <kvnadesara@gmail.com>
2016-06-29 18:27:29 +0530

    use express-formidable for multipart file upload

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 /**
  * Module dependencies.
  */
-var express  = require('express');
-var connect = require('connect');
-var app      = express();
-var port     = process.env.PORT || 3000;
+var express    = require('express');
+var connect    = require('connect');
+var formidable = require('express-formidable');
+var app        = express();
+var port       = process.env.PORT || 3000;
 
 // Configuration
 app.use(express.static(__dirname + '/public'));
 app.use(connect.cookieParser());
 app.use(connect.logger('dev'));
-app.use(connect.bodyParser({ keepExtensions: true, uploadDir: "/Users/kevin.adesara/media-upload/" }));
-app.use(connect.multipart({ limit: '1mb' }));
-
-app.use(connect.json());
+//app.use(connect.bodyParser());
+app.use(formidable.parse({ keepExtensions: true, uploadDir: "/Users/kevin.adesara/media-upload/" }));
 app.use(connect.urlencoded());
+app.use(connect.json());
 
 // Routes
 
 require('./routes/routes.js')(app);
-
 
 app.listen(port);
 console.log('The App runs on port ' + port);
